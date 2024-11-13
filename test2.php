376cf228d7b6528ddc85ed65537b9e344127d26e
AFYN-happy <puntyo2323@gmail.com>
2024-11-13 15:31:39 +0900

    test2に記載

---
+++ test2.php
@@ -0,0 +1,26 @@
+<?php
+$day = "Monday";
+switch ($day) {
+    case "Monday":
+        echo "月曜日";
+        break;
+    case "Tuesday":
+        echo "火曜日";
+        break;
+    case "Wednesday":
+        echo "水曜日";
+        break;
+    case "Thursday":
+        echo "木曜日";
+        break;
+    case "Friday":
+        echo "金曜日";
+        break;
+    case "Saturday":
+        echo "土曜日";
+        break;
+    case "Sunday":
+        echo "日曜日";
+        break;
+}
+?>
