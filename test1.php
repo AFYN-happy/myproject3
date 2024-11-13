12d99ddf08ace94bad152cac441d1ba8086f295b
AFYN-happy <puntyo2323@gmail.com>
2024-11-13 14:54:43 +0900

    test1を追加

---
+++ test1.php
@@ -0,0 +1,13 @@
+<?php
+$score = 70;
+if ($score >= 90) {
+    echo "A";
+} elseif ($score >= 80) {
+    echo "B";
+} elseif ($score >= 70) {
+    echo "C";
+} elseif ($score >= 60) {
+    echo "D";
+} else {
+    echo "F";
+}
